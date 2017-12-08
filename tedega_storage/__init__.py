e6c39216bf56fcf69ce4872004473e2c76d55467
Torsten Irländer <torsten.irlaender@googlemail.com>
2017-12-08 10:25:12 +0100

    Removed global imports.

--- tedega_storage/__init__.py
+++ tedega_storage/__init__.py
@@ -1,8 +1,4 @@
 # -*- coding: utf-8 -*-
-from .rdbms.base import init_storage, get_storage, RDBMSStorageBase
-
-__all__ = [init_storage, get_storage, RDBMSStorageBase]
-
 __author__ = """Torsten Irländer"""
 __email__ = 'torsten.irlaender@googlemail.com'
 __version__ = '0.1.0'
